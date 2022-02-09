6f7856e6f651da88a16e59590fb937f297fb4591
deano <77565607+deanOcoin@users.noreply.github.com>
2022-02-09 00:28:02 -0500

    Add files via upload

---
+++ bootsector.asm
@@ -0,0 +1,33 @@
+use16
+org 0x7c00
+
+; Clear screen
+mov ax, 3 ; video mode = 3
+int 10h
+;
+
+
+mov si, github
+mov ah, 0xE
+
+displaychar:
+	mov al, [si] ; store value of si in al
+	inc si
+	cmp al, 0
+	je stopdisplay
+	; Display current cycled character
+	int 0x10
+	jmp displaychar
+	
+
+stopdisplay:
+	cli
+	hlt ; stop system
+
+github: db "github.com/deanOcoin",0
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
+
+
